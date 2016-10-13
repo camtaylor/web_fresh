171bc4163b1c26a2cb9ed55ec8384e1b7f009220
camtaylor <camerontaylor00@gmail.com>
2016-10-13 11:55:29 -0700

    Added color palette

--- scraper.py
+++ scraper.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+from PIL import Image
+from StringIO import StringIO
+
 
 
 logo = [] 
 links_visited = 0
+
 
 def get_navbar_links(url):
   global navbar_links
@@ -15,9 +19,11 @@ def get_navbar_links(url):
   if len(navbar) > 0:
     for li in navbar[0].find_all('li'):
       navbar_links.append(li.find('a'))
-  print navbar_links 
-  return [(link.string,link['href']) for link in navbar_links if link and link.has_attr('href')]
-    
+  try:
+    return [(link.string,link['href']) for link in navbar_links if link and link.has_attr('href')]
+  except Exception as e:
+    print e 
+    return [] 
   
 def get_logo(url):
   html = requests.get(url)
@@ -27,11 +33,24 @@ def get_logo(url):
     logos.extend(link.find_all('img'))
   if len(logos) == 0:
     logos = soup.find_all('img')
-  print logos
-  return logos[0]['src']    
+  return logos[0]['src']
  
 
-if __name__ == "__main__":
+def rgb2hex(r, g, b):  
+  return '#{:02x}{:02x}{:02x}'.format(r, g, b)  
 
-  print get_logo(raw_input(">>>"))
+def get_colors(image_url):
+  response = requests.get(image_url)
+  img = Image.open(StringIO(response.content))
+  img_rgb = img.convert('RGB')
+  colors = img_rgb.getcolors()
+  try:
+    return [rgb2hex(*color[1]) for color in colors if color[0] > 100] 
+  except Exception as e:
+    print e
+    return []
+  
+if __name__ == "__main__":
+  
+  print get_colors(get_logo("http://www.francolawgroup.com"))
     
--- web_fresh.py
+++ web_fresh.py
@@ -2,6 +2,9 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 import scraper
 import requests
+from PIL import Image
+from StringIO import StringIO
+
 
 @app.route("/", methods=["GET"])
 def map():
@@ -10,8 +13,8 @@ def map():
   if "http" not in logo:
     logo = url + logo
   navbar_links = scraper.get_navbar_links(url)
-  print navbar_links
-  return render_template("template.html", logo=logo, navbar_links=navbar_links)
+  colors = scraper.get_colors(logo)
+  return render_template("template.html", logo=logo, navbar_links=navbar_links, colors=colors)
 
 
 if __name__ == "__main__":
